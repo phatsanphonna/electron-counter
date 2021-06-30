97fbbeeb2e310c962951f3305d60ecc58823b5df
ssuniie <phatsanphon2004@outlook.com>
2021-06-30 11:01:09 +0700

    add goal statements

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
-const clickBtn = document.getElementById('clickBtn')
-const number = document.getElementById('number')
+const clickBtn = document.getElementById('clickBtn');
+const number = document.getElementById('number');
+const desc = document.getElementById('desc');
 
 clickBtn.addEventListener('click', () => {
-    let numberCount = parseInt(number.innerHTML)
-    numberCount++
-    number.innerHTML = numberCount
-    console.log(number.innerHTML)
-})
+    let numberCount = parseInt(number.innerHTML);
+
+    numberCount++;
+    number.innerHTML = numberCount;
+
+    switch (numberCount) {
+        case 10:
+            desc.innerHTML = 'You have clicked 10 times already.'
+            break
+        case 100:
+            desc.innerHTML = 'Great! You have clicked 100 times already.'
+            break
+        case 200:
+            desc.innerHTML = 'Excellent! You have clicked 200 times already.'
+            break
+    }
+
+    console.log(number.innerHTML);
+});
